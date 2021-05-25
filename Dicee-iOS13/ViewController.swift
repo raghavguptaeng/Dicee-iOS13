fb8f0e586f05bf3f671376755c1f0ce4cefd2098
Raghav Gupta <E19CSE258@bennett.edu.in>
2021-05-24 21:20:54 -0700

    Left Dice Made serial

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,15 +12,32 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImage1: UIImageView!
     @IBOutlet weak var diceImage2: UIImageView!
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 6
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func RollDice(_ sender: UIButton) {
-        //let temp = diceImage1
-        diceImage1.image = diceImage2.image
-        diceImage2.image = #imageLiteral(resourceName: "DiceTwo")
+        if(leftDiceNumber<5){
+            leftDiceNumber = leftDiceNumber+1
+        }
+        else{
+            leftDiceNumber = 0
+        }
+        diceImage1.image = [
+            #imageLiteral(resourceName: "DiceOne"),
+            #imageLiteral(resourceName: "DiceTwo"),
+            #imageLiteral(resourceName: "DiceThree"),
+            #imageLiteral(resourceName: "DiceFour"),
+            #imageLiteral(resourceName: "DiceFive"),
+            #imageLiteral(resourceName: "DiceSix")
+        
+        ][leftDiceNumber]
+        
     }
     
 }
