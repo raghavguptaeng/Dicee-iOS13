ed54ed164031fff7b30210955bd495ebd2867b9e
Raghav Gupta <E19CSE258@bennett.edu.in>
2021-05-24 21:07:28 -0700

    UI Components connected to backend

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func RollDice(_ sender: UIButton) {
+        //let temp = diceImage1
+        diceImage1.image = diceImage2.image
+        diceImage2.image = #imageLiteral(resourceName: "DiceTwo")
+    }
+    
 }
 
